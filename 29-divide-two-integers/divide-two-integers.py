604c6a3f8e78244b4a85237f0c499a9f8b5e809d
Pranay Nagpure <pranay18nagpure@gmail.com>
2023-09-25 18:14:38 +0530

    Time: 35 ms (87.33%) | Memory: 16.3 MB (29.94%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.py
@@ -0,0 +1,19 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        positive = (dividend < 0) is (divisor < 0) 
+        dividend = abs(dividend)
+        divisor =  abs(divisor) 
+        res = 0 
+        while dividend >= divisor: 
+                curr_divisor, num_divisors = divisor, 1 
+                while dividend >= curr_divisor: 
+                    dividend -= curr_divisor 
+                    res += num_divisors 
+                    curr_divisor = curr_divisor << 1 
+                    num_divisors = num_divisors << 1 
+                    
+        if not positive: 
+            res = -res 
+            
+        return min(max(-2147483648, res), 2147483647) 
+        
