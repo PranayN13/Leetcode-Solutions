1ddeed527cedd5e18284ed6404b1b47421f9a764
Pranay Nagpure <pranay18nagpure@gmail.com>
2023-10-07 22:52:32 +0530

    Time: 95 ms (92.92%) | Memory: 16.1 MB (100.00%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -1,12 +1,24 @@
 class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
-        if m < k: return 0
+        if m < k:
+            return 0
+        
+        # Initialize a 2D array for dynamic programming
+        # dp[i][j] represents the number of ways to construct an array of length i+1 with maximum element j+1
         dp = [[1] * m] + [[0] * m for _ in range(k - 1)]
+        
         mod = 10 ** 9 + 7
+        
+        # Iterate through the elements
         for _ in range(n - 1):
             for i in range(k - 1, -1, -1):
                 cur = 0
                 for j in range(m):
+                    # Update dp[i][j] using the given recurrence relation
                     dp[i][j] = (dp[i][j] * (j + 1) + cur) % mod
-                    if i: cur = (cur + dp[i - 1][j]) % mod
-        return sum(dp[-1]) % mod
+                    if i:
+                        cur = (cur + dp[i - 1][j]) % mod
+        
+        # Calculate the total number of arrays for length n with at most m as the maximum element
+        total_arrays = sum(dp[-1]) % mod
+        return total_arrays
